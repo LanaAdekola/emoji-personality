5287b2c3f771b32611f3d9d65ed61ed40f8afe55
LanaAdekola <adekolaadelana@gmail.com>
2023-01-20 13:38:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const emojiContainer = document.getElementById("emoji-container")
 const emojiInput = document.getElementById("emoji-input")
 const pushBtn = document.getElementById("push-btn")
 const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
 function renderEmojis() {
     emojiContainer.innerHTML = ""
@@ -30,4 +32,13 @@ unshiftBtn.addEventListener("click", function(){
         emojiInput.value = ""
         renderEmojis()
     }
+})
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
 })
