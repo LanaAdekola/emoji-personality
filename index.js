7f8205013b2dcaa6128d05a41efd74548a6788a5
LanaAdekola <adekolaadelana@gmail.com>
2023-01-20 11:48:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-fg
+
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emoji-container")
+
+for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+}
