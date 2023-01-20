ffc29173017131bb89c4a45d72e14c00c4a4cdf9
LanaAdekola <adekolaadelana@gmail.com>
2023-01-20 13:07:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
 
-for (let i = 0; i < myEmojis.length; i++) {
-    const emoji = document.createElement('span')
-    emoji.textContent = myEmojis[i]
-    emojiContainer.append(emoji)
+function renderEmojis() {
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
 }
+
+renderEmojis()
+
+pushBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
